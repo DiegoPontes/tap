24fc814e5223eb1ab13eaa2c73d8bdfcb7f72505
Diego Pontes <dpontes@indracompany.com>
2020-09-01 21:24:26 -0300

    # Questões resolvidas do URI (1171 e 1895).

---
+++ URI/1171/1171/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1171
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> valoresEntrada = new List<int>();
+            List<int> valoresUnicos = new List<int>();
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                valoresEntrada.Add(int.Parse(Console.ReadLine()));
+            }
+
+            valoresUnicos.AddRange(valoresEntrada.OrderBy(x => x).Distinct());
+
+            for (int i = 0; i < valoresUnicos.Count; i++)
+            {
+                
+                Console.WriteLine($"{valoresUnicos[i]} aparece {valoresEntrada.Where(x => x == valoresUnicos[i]).Count()} vez(es)");
+            }
+        }
+    }
+}
---
+++ URI/1895/1895/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1895
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ntl = Console.ReadLine();
+            string[] valores = ntl.Split(' ');
+            int n = int.Parse(valores[0]),
+                t = int.Parse(valores[1]),
+                l = int.Parse(valores[2]),
+                alice = 0,
+                bob = 0;
+
+            bool vezAlice = true;
+
+            for (int i = 1; i < n; i++)
+            {
+                int carta = int.Parse(Console.ReadLine());
+                if (Math.Abs(t - carta) <= l)
+                {
+                    if (vezAlice)
+                    {
+                        alice += Math.Abs(t - carta);
+                    }
+                    else
+                    {
+                        bob += Math.Abs(t - carta);
+                    }
+
+                    t = carta;
+                }
+                vezAlice = !vezAlice;
+            }
+
+            Console.WriteLine(alice + " " + bob);
+        }
+    }
+}
