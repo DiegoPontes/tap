9bbb5ae0f76b7deeb989f925f6552698658feb6a
Diego Pontes <dpontes@indracompany.com>
2020-09-08 12:01:40 -0300

    # Questões resolvidas do URI (1222 e 1533).

---
+++ URI/1222/1222/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> r = new List<int>();
+            bool x = true;
+            while (x)
+            {
+                string value = Console.ReadLine();
+                if (string.IsNullOrEmpty(value))
+                {
+                    x = false;
+                    break;
+                }
+
+                string[] nlc = value.Split(' ');
+                int n = int.Parse(nlc[0]),
+                    l = int.Parse(nlc[1]),
+                    c = int.Parse(nlc[2]);
+
+                int linhas = 0,
+                    caracteres = 0;
+
+                string[] txt = Console.ReadLine().Split(' ');
+
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    caracteres += txt[i].Length;
+
+                    if (caracteres > c)
+                    {
+                        linhas++;
+                        caracteres = txt[i].Length + 1;
+                    }
+                    else if (caracteres == c || caracteres + 1 == c)
+                    {
+                        linhas++;
+                        caracteres = 0;
+                    }
+                    else
+                    {
+                        caracteres++;
+                    }
+                }
+
+                if (caracteres > 0)
+                {
+                    linhas++;
+                }
+
+                int tl = linhas / l;
+
+                if (linhas % l > 0)
+                {
+                    tl++;
+                }
+
+                r.Add(tl);
+            }
+
+            for (int i = 0; i < r.Count; i++)
+            {
+                Console.WriteLine(r[i]);
+            }
+        }
+    }
+}
---
+++ URI/1533/1533/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1533
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            int n = int.Parse(Console.ReadLine());
+            
+            while (n != 0)
+            {
+                if (n == 0) { break; }
+
+                int[] s = new int[n];
+                int[] aux = new int[n];
+
+                string[] suspeitos = Console.ReadLine().Split(' ');
+
+                for (int i = 0; i < n; i++)
+                {
+                    s[i] = int.Parse(suspeitos[i]);
+                }
+
+                Array.Copy(s, aux, n);
+                Array.Sort(aux, 0, n);
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    if (aux[n - 2] == s[i])
+                    {
+                        a.Add(i + 1);
+                        break;
+                    }
+                }
+
+                n = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
